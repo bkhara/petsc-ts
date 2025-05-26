a0c0e7e66175af5f8c4fb615cf013841e569f460
biswajit <biswajitkhara@gmail.com>
2025-05-26 16:12:06 -0500

    Added a pseudo time marching for the heat equation in 2d

---
+++ xrun/cases/heat2d_ptc/runex.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+NPROC=4
+OUTPUT_FILE="out.txt"
+FEXEC="heat2d_ptc"
+
+if [ ! -d "./datafiles" ]; then
+    mkdir -p "./datafiles"
+fi
+
+
+# {PROJECT_DIR} will be replaced by the python script
+BUILD_DIR="/work/mech-ai/bkhara/petsc-ts/build"
+cd $BUILD_DIR || exit; make ${FEXEC}; cd - || exit
+
+
+mpirun -n ${NPROC} ${BUILD_DIR}/${FEXEC} > ${OUTPUT_FILE} 2>&1 \
+-options_view \
+-da_refine 5 \
+-ts_monitor_solution_vtk datafiles/blah-%03D.vts \
+-ts_type pseudo \
+-ts_monitor_pseudo \
+-ts_pseudo_frtol 1e-6 \
+# -ts_pseudo_increment 2 \
+# -ts_pseudo_increment_dt_from_initial_dt
+# -ts_max_time 200 \
+# -ts_monitor \
+#-ts_dt 0.001 \
+#-ts_save_trajectory
+#-ts_bdf_order 2 \
+#-ts_adapt_type none \
